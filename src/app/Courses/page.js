5327d553b2a456bcff94d996a8ec4d4541358d2b
Adham Sherif Abd El-Hamid <75391774+adhamsherif76262@users.noreply.github.com>
2024-08-07 16:04:14 +0300

    Update page.js

--- src/app/Courses/page.js
+++ src/app/Courses/page.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import CoursesSection from './CoursesSection/page';
 async function Courses() {
 
-  const CourseIntroResponse = await axios.get(`http://localhost:5000/Array`)
+  // const CourseIntroResponse = await axios.get(`http://localhost:5000/Array`)
     const courseIntro = CourseIntroResponse.data
 
-    const TabsResponse = await axios.get(`http://localhost:5000/HomePageTaps`)
+    // const TabsResponse = await axios.get(`http://localhost:5000/HomePageTaps`)
     const Tabs = TabsResponse.data
     return (
       <>
